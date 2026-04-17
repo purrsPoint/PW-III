fbd3392417517cacb1a2e392bc3c2d033fa41419
purrsPoint <felipedossantosleonardo@gmail.com>
2026-04-17 11:40:13 -0300

    ta funcionando com a api Gg go next

---
+++ editoremweb/config/executecode.php
@@ -0,0 +1,97 @@
+<?php
+
+function execode($aluno_code)
+{
+
+    $full_code = "\npublic class Main {\n";
+    $full_code .= "     public static void main(String[] args){\n";
+
+    //onde o codigo do aluno vai
+    $full_code .= "         " . $aluno_code . "\n";
+
+    $full_code .= "     }\n";
+    $full_code .= "}";
+
+    //url da api
+    $url = "https://ce.judge0.com/submissions";
+
+    //oque vai mandar pra api 
+    //id62 é o do java
+    //source_code precisa ser base64 pq a api qr
+
+    $dados = [
+        "language_id" => 62,
+        "source_code" => $full_code,
+        "base64_encoded" => false
+    ];
+
+    //preparando o post, são as regras de como vai mandar basicamente
+    $options = [
+        "http" => [
+            // \r\n é a forma correta de quebrar pq http é veio e estranho
+            "header" => "Content-Type: application/json\r\n",
+            "method" => "POST",
+            "content" => json_encode($dados)
+        ]
+    ];
+
+    //tranforma as regras em algo utilizavel
+    $contextenv = stream_context_create($options);
+
+    //envia pro sv o codigo dps de ser passado 
+    $resultado = file_get_contents($url, false, $contextenv);
+
+    if ($resultado === FALSE) {
+        return "erro na hora de conectar com a api";
+    }
+
+    //retorna o resultado da api
+    $respostaapi = json_decode($resultado, true);
+
+    $token = $respostaapi["token"] ?? null;
+
+    if (!$token) {
+        return "erro: nao veio token da api";
+    }
+
+    //buscar resultado da execução
+    $url_result = "https://ce.judge0.com/submissions/$token?base64_encoded=false";
+
+    $max_tries = 10;
+    $try = 0;
+
+    do {
+
+        $final = file_get_contents($url_result);
+
+        if ($final === FALSE) {
+            return "erro ao buscar resultado da execucao";
+        }
+
+        $dadosfinal = json_decode($final, true);
+
+        $status = $dadosfinal["status"]["id"] ?? 0;
+
+        // 1 = in queue
+        // 2 = processing
+        if ($status <= 2) {
+            sleep(1);
+        }
+
+        $try++;
+
+    } while ($status <= 2 && $try < $max_tries);
+
+    //erro na hora de compilar
+    if (!empty($dadosfinal["compile_output"])) {
+        return "Erro de compilação:\n" . $dadosfinal["compile_output"];
+    }
+
+    //erro de execução
+    if (!empty($dadosfinal["stderr"])) {
+        return "Erro de execução:\n" . $dadosfinal["stderr"];
+    }
+
+    //retorna output
+    return $dadosfinal["stdout"] ?? "sem saída.";
+}
--- editoremweb/pages/index.php
+++ editoremweb/pages/index.php
@@ -1,59 +1,43 @@
 <?php
 require_once '../config/config.php';
+require_once __DIR__ . '/../config/executecode.php';
 
 $message = "";
 $output = "";
 $aluno_code = "";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['java_code'])) {
-    $aluno_code = $_POST['java_code'];
 
-    // Montagem do código completo
-    $full_code = "public class Main {\n";
-    $full_code .= "    public static void main(String[] args) {\n";
-    $full_code .= "        " . $aluno_code . "\n";
-    $full_code .= "    }\n";
-    $full_code .= "}";
+    $aluno_code = $_POST['java_code'] ?? "";
 
-    $filepath = JAVA_TEMP_DIR . "Main.java";
-
-    if (file_put_contents($filepath, $full_code)) {
-        // Compila e Roda
-        $compile = shell_exec("javac \"$filepath\" 2>&1");
-        if ($compile) {
-            $output = "Erro de Compilação:\n" . $compile;
-        } else {
-            $output = shell_exec("java -cp \"" . JAVA_TEMP_DIR . "\" Main 2>&1");
-        }
-        
-        // Limpa os rastros
-       @unlink($filepath);
-       @unlink(JAVA_TEMP_DIR . "Main.class");
-       
-    }
+    //envia pra api
+    $output = execode($aluno_code);
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <title>Java School | Editor</title>
     <link rel="stylesheet" href="../assets/css/style.css">
 </head>
+
 <body>
 
-    <h1>Editor de Java 🚀</h1>
+    <h1>Editor de Java</h1>
     <p><?php echo $message; ?></p>
 
     <div class="editor-container">
         <form method="POST">
             <code class="fixed-code">public class Main {</code>
             <code class="fixed-code indent">public static void main(String[] args) {</code>
-            
+
             <?php // o htmlespecialchars faz com q os caracteres especiais sejam interpretados de outra forma ent n da uns certos erros ?>
-            <textarea name="java_code" rows="12" placeholder="// Escreva seu código aqui..."><?php echo htmlspecialchars($aluno_code); ?></textarea>
-            
+            <textarea name="java_code" rows="12"
+                placeholder="// Escreva seu código aqui..."><?php echo htmlspecialchars($aluno_code); ?></textarea>
+
             <code class="fixed-code indent">}</code>
             <code class="fixed-code">}</code>
             <br>
@@ -63,10 +47,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['java_code'])) {
 
     <?php if ($output): ?>
         <div class="console">
-            <strong>Console:</strong><br>
-            <?php echo htmlspecialchars($output); ?>
+            <pre><?php echo htmlspecialchars($output); ?></pre>
         </div>
     <?php endif; ?>
 
 </body>
+
 </html>
